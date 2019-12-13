01a25f7e94f4476420852b95eec880fa9d6ee6ca
Franek <panczo.mirecki@gmail.com>
2019-12-13 19:17:27 +0100

    middle of stage #2
    kind of working, need to debug

--- src/recognition/NeuralNetwork.java
+++ src/recognition/NeuralNetwork.java
@@ -2,10 +2,30 @@ package recognition;
 
 public class NeuralNetwork {
 
-    private int[] a;
+    private int[] inputNeurons;
+    private int[] outputNeurons;
     private int[][] weights;
+    private int[] biases;
 
-    NeuralNetwork() {
+    NeuralNetwork(int[] iN, int oN, int[][] w, int[] b) {
+        inputNeurons = iN;
+        outputNeurons = new int[oN];
+        weights = w;
+        biases = b;
+    }
 
+    int evaluate() {
+        int output = -100000000;
+        for (int i = 0; i < outputNeurons.length; i++) {
+            for (int j = 0; j < inputNeurons.length; j++) {
+                outputNeurons[i] += (inputNeurons[i] * weights[i][j]);
+            }
+            outputNeurons[i] += biases[i];
+            if(outputNeurons[i] > output){
+                output = outputNeurons[i];
+            }
+        }
+
+        return output;
     }
 }
